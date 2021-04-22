f5ba5269713614fdab9f1f0319b7abe9f03b69ab
jjjjonathan <hornathanjon@gmail.com>
2021-04-22 01:21:16 -0500

    exercise 8.8 complete

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+import {
+  ApolloClient,
+  ApolloProvider,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import { ALL_AUTHORS } from './queries';
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: new HttpLink({
+    uri: 'http://localhost:4000',
+  }),
+});
+
+client.query({ query: ALL_AUTHORS }).then((response) => {
+  console.log(response.data);
+});
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')
+);
