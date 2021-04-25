64653d4580a44c7010d1d8f6af84e42d3bca666b
jjjjonathan <hornathanjon@gmail.com>
2021-04-24 19:05:39 -0500

    exercise 8.19 complete

--- client/src/components/Books.js
+++ client/src/components/Books.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useQuery } from '@apollo/client';
 import { ALL_BOOKS } from '../queries';
 
 const Books = (props) => {
+  const [genreFilter, setGenreFilter] = useState('');
+
   const query = useQuery(ALL_BOOKS);
 
   if (!props.show) {
@@ -15,26 +17,60 @@ const Books = (props) => {
 
   const books = query.data.allBooks;
 
+  let genres = [];
+  books.forEach((book) => {
+    book.genres.forEach((genre) => {
+      if (!genres.includes(genre)) {
+        genres.push(genre);
+      }
+    });
+  });
+
   return (
     <div>
       <h2>books</h2>
-
-      <table>
+      {genreFilter ? (
+        <p>
+          in genre <strong>{genreFilter}</strong>
+        </p>
+      ) : null}
+      <table style={{ marginBottom: 15 }}>
         <tbody>
           <tr>
             <th></th>
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
-            <tr key={a.id}>
-              <td>{a.title}</td>
-              <td>{a.author.name}</td>
-              <td>{a.published}</td>
-            </tr>
-          ))}
+          {books
+            .filter((book) =>
+              genreFilter ? book.genres.includes(genreFilter) : true
+            )
+            .map((a) => (
+              <tr key={a.id}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            ))}
         </tbody>
       </table>
+      <button
+        onClick={() => {
+          setGenreFilter('');
+        }}
+      >
+        <strong>view all</strong>
+      </button>
+      {genres.map((genre) => (
+        <button
+          key={genre}
+          onClick={() => {
+            setGenreFilter(genre);
+          }}
+        >
+          {genre}
+        </button>
+      ))}
     </div>
   );
 };
