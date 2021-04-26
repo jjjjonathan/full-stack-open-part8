b54e9eb11c3e9996c223d2d9a3f202a8c3034048
jjjjonathan <hornathanjon@gmail.com>
2021-04-25 22:36:38 -0500

    exercise 8.23 complete

--- client/src/components/NewBook.js
+++ client/src/components/NewBook.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { ADD_BOOK, ALL_AUTHORS, ALL_BOOKS, BOOKS_BY_GENRE } from '../queries';
+import { ADD_BOOK, ALL_AUTHORS, ALL_BOOKS } from '../queries';
 
 const NewBook = ({ setPage, show }) => {
   const [title, setTitle] = useState('');
